6fff18f173843e6ed79af35765b7b17bf5e97ae3
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-23 09:23:45 -0600

    Checkbox created

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+let DarkTheme = true
+const $Button = document.getElementById('theme-switcher')
+
+$Button.addEventListener('click', e => {
+	if (DarkTheme) {
+		document.documentElement.style.setProperty('--bg', '#ffffff')
+		document.documentElement.style.setProperty('--topBgPattern', '#f5f7ff')
+		document.documentElement.style.setProperty('--cardBg', '#f0f2fa')
+		document.documentElement.style.setProperty('--desaturatedText', '#63687e')
+		document.documentElement.style.setProperty('--text', '#1e202a')
+		DarkTheme = false
+	} else {
+		document.documentElement.style.setProperty('--bg', '#1e202a')
+		document.documentElement.style.setProperty('--topBgPattern', '#1f212e')
+		document.documentElement.style.setProperty('--cardBg', '#252a41')
+		document.documentElement.style.setProperty('--desaturatedText', '#8b97c6')
+		document.documentElement.style.setProperty('--text', '#ffffff')
+		DarkTheme = true
+	}
+})
