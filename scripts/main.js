3f3718cd2620767d37acc4f9a32e2428a0968b4e
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-25 10:57:32 -0600

    Stats cards completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-let DarkTheme = true
 const $Switch = document.getElementById('switch')
 
 $Switch.addEventListener('click', e => {
@@ -8,13 +7,11 @@ $Switch.addEventListener('click', e => {
 		document.documentElement.style.setProperty('--cardBg', '#f0f2fa')
 		document.documentElement.style.setProperty('--desaturatedText', '#63687e')
 		document.documentElement.style.setProperty('--text', '#1e202a')
-		DarkTheme = false
 	} else {
 		document.documentElement.style.setProperty('--bg', '#1e202a')
 		document.documentElement.style.setProperty('--topBgPattern', '#1f212e')
 		document.documentElement.style.setProperty('--cardBg', '#252a41')
 		document.documentElement.style.setProperty('--desaturatedText', '#8b97c6')
 		document.documentElement.style.setProperty('--text', '#ffffff')
-		DarkTheme = true
 	}
 })
