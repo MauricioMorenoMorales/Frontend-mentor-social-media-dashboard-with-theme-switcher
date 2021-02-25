c9a468933a33b099021264b4455c0f4350869e21
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-25 13:02:07 -0600

    Completed

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,14 @@ $Switch.addEventListener('click', e => {
 		document.documentElement.style.setProperty('--bg', '#ffffff')
 		document.documentElement.style.setProperty('--topBgPattern', '#f5f7ff')
 		document.documentElement.style.setProperty('--cardBg', '#f0f2fa')
+		document.documentElement.style.setProperty('--cardBgHover', '#e1e3f0')
 		document.documentElement.style.setProperty('--desaturatedText', '#63687e')
 		document.documentElement.style.setProperty('--text', '#1e202a')
 	} else {
 		document.documentElement.style.setProperty('--bg', '#1e202a')
 		document.documentElement.style.setProperty('--topBgPattern', '#1f212e')
 		document.documentElement.style.setProperty('--cardBg', '#252a41')
+		document.documentElement.style.setProperty('--cardBgHover', '#333a56')
 		document.documentElement.style.setProperty('--desaturatedText', '#8b97c6')
 		document.documentElement.style.setProperty('--text', '#ffffff')
 	}
